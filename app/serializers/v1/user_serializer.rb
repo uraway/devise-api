b7a0051f236b918bea488cc48a844159dfe10aeb
後閑諒一 <nargok@gmail.com>
2017-09-02 17:10:47 +0900

    nameをjsonパラメータに追加

--- app/serializers/v1/user_serializer.rb
+++ app/serializers/v1/user_serializer.rb
@@ -1,5 +1,5 @@
 module V1
   class UserSerializer < ActiveModel::Serializer
-    attributes :id, :email, :created_at, :updated_at
+    attributes :id, :email, :name, :created_at, :updated_at
   end
 end
